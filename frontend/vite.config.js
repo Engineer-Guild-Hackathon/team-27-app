1259c72b0e8a743c8f5d892b6e3b49215dff2cec
otoneko1102 <pineapple2006.11.2@gmail.com>
2025-09-09 15:44:11 +0900

    Create vite config for build

---
+++ frontend/vite.config.js
@@ -0,0 +1,29 @@
+import { defineConfig, loadEnv } from "vite";
+import { createHtmlPlugin } from "vite-plugin-html";
+
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+
+  return {
+    root: "./routes",
+    server: {
+      port: Number(env.PORT) || 5173,
+    },
+    build: {
+      outDir: "./build",
+      emptyOutDir: true,
+      rollupOptions: {
+        output: {
+          entryFileNames: "assets/[hash].js",
+          chunkFileNames: "assets/[hash].js",
+          assetFileNames: (i) => {
+            if (/\.(a?png|jpe?g|gif|webp|mp3|m4a|mp4|mov|webm)$/.test(i.name)) {
+              return "assets/img/[name][extname]"; // Image / Video
+            }
+            return "assets/[hash][extname]";
+          },
+        },
+      },
+    },
+  }
+});
