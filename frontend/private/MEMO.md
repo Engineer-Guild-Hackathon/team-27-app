f196c44936bae938933fff2a5e4ea977a19573d3
otoneko1102 <pineapple2006.11.2@gmail.com>
2025-09-10 11:02:36 +0900

    Fix plugin.

---
+++ frontend/private/MEMO.md
@@ -0,0 +1,8 @@
+git branch(dev): frontend-dev
+
+## TO DO after git pull
+
+```bash
+cd frontend
+npm install
+```
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,27 +1,27 @@
+import { resolve, basename } from "path";
+import fs from "fs";
 import { defineConfig, loadEnv } from "vite";
-import { createHtmlPlugin } from "vite-plugin-html";
+import { createMultiHtmlPlugin } from "vite-plugin-multi-html";
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), "");
 
   return {
     root: "./routes",
+    // Dev server
     server: {
       port: Number(env.PORT) || 5173,
     },
     plugins: [
-      createHtmlPlugin({
-        minify: true,
-        inject: {
-          data: {
-            lang: env.VITE_LANG,
-            name: env?.VITE_NAME,
-          },
-        },
-      }),
+      createMultiHtmlPlugin({
+        variables: {
+          lang: env.VITE_LANG,
+          name: env.VITE_NAME
+        }
+      })
     ],
     build: {
-      outDir: "./build",
+      outDir: "../build",
       emptyOutDir: true,
       rollupOptions: {
         output: {
@@ -29,12 +29,12 @@ export default defineConfig(({ mode }) => {
           chunkFileNames: "assets/[hash].js",
           assetFileNames: (i) => {
             if (/\.(a?png|jpe?g|gif|webp|mp3|m4a|mp4|mov|webm)$/.test(i.name)) {
-              return "assets/img/[name][extname]"; // Image / Video
+              return "assets/img/[name][extname]";
             }
             return "assets/[hash][extname]";
           },
         },
       },
     },
-  }
+  };
 });
